9a5f6f9d1c31f470cc1d022e6430481a620ec137
qianfan <qianfan@163.com>
2021-04-30 09:56:44 +0800

    update UserMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -58,4 +58,15 @@ public interface UserMapper {
         0.测试从远程仓库pull操作
      */
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
+
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
+    public void test8();
+    public void test9();
+    public void test10();
 }
