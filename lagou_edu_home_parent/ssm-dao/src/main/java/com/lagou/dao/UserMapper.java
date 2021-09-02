cd0e5fa7710d0a1866e3d3d26d91fc8bda93c6a5
chengqianfan <18813050563@163.com>
2021-09-02 22:19:24 +0800

    first commit

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -70,14 +70,5 @@ public interface UserMapper {
     public void test90();
     public void test100();
 
-    public void test1();
-    public void test2();
-    public void test3();
-    public void test4();
-    public void test5();
-    public void test6();
-    public void test7();
-    public void test8();
-    public void test9();
 
 }
